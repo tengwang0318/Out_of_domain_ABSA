b86a4632817e784a1ae4b6b715a4ab0d0fa5363f
teng wang <wt0318@outlook.com>
2021-11-12 21:02:03 +0800

    Add files via upload

---
+++ scripts/data_loader.py
@@ -0,0 +1,51 @@
+from torch.utils.data import Dataset
+from transformers import BertModel, AutoTokenizer, AutoModelWithLMHead, AutoModel
+from tqdm import tqdm
+import torch
+import pandas as pd
+
+
+class DataClass(Dataset):
+    def __init__(self, args, file_name):
+        self.args = args
+        self.file_name = file_name
+        self.max_length = int(args['--max-length'])
+        self.sentences, self.aspects, self.labels = self.load_dataset()
+        if args['--bert_type'] == 'base-bert':
+            self.tokenizer = AutoTokenizer.from_pretrained('bert-base-uncased')
+        elif args['--bert_type'] == 'DeBerta':
+            self.tokenizer = AutoTokenizer.from_pretrained("microsoft/deberta-base")
+        elif args['--bert_type'] == 'T5':
+            self.tokenizer = AutoTokenizer.from_pretrained("t5-base")
+        self.inputs = self.process_data()
+        self.labels = torch.tensor(self.labels)
+
+    def load_data(self):
+        df = pd.read_csv(self.file_name, delimiter='\t')
+        sentences, aspects, labels = df.sentence.values, df.category_polarity.values, df.entailed.values
+        return sentences, aspects, labels
+
+    def process_data(self):
+        desc = "Preprocessing dataset {}...".format("")
+        inputs = []
+        for x, aspect in tqdm(zip(self.sentences, self.aspects), desc=desc):
+            x = self.tokenizer.encode_plus(
+                aspect,
+                x,
+                add_special_tokens=True,
+                max_length=self.max_length,
+                pad_to_max_length=True,
+                truncation=True
+            )
+            input_id = x['input_ids']
+            inputs.append(input_id)
+        inputs = torch.tensor(inputs, dtype=torch.long)
+        return inputs
+
+    def __getitem__(self, index):
+        inputs = self.inputs[index]
+        labels = self.labels[index]
+        return inputs, labels
+
+    def __len__(self):
+        return len(self.inputs)
---
+++ scripts/model.py
@@ -0,0 +1,64 @@
+""" we can use base-bert, T5, DeBerta as our model encoder architecture"""
+from transformers import BertModel, AutoTokenizer, AutoModelWithLMHead, AutoModel
+import torch.nn.functional as F
+import torch.nn as nn
+import torch
+import transformers
+
+
+class BertEncoder(nn.Module):
+    def __init__(self, model_type="base-bert"):
+        """
+        :model_type str: model architecture
+        """
+        super(BertEncoder, self).__init__()
+        if model_type == 'base-bert':
+            self.bert = BertModel.from_pretrained("bert-base-uncased")
+        elif model_type == 'T5':
+            self.bert = AutoModelWithLMHead.from_pretrained("t5-base")
+        elif model_type == "DeBerta":
+            self.bert = AutoModel.from_pretrained('microsoft/deberta-base')
+        self.feature_size = self.bert.config.hidden_size
+
+    def forward(self, input_ids):
+        """
+
+        :param input_ids: List[int], the id of every "token" in the sentence
+        :return: the last hidden state, the shape is (batch_size, seq_length, hidden_size)
+        """
+        last_hidden_state = self.bert(input_ids=input_ids).last_hidden_state
+        return last_hidden_state
+
+
+class OodModel(nn.Module):
+    """the model will do aspect based sentiment out of domain"""
+
+    def __init__(self, output_dropout=.1, model_type='base-bert'):
+        super(OodModel, self).__init__()
+        self.bert = BertEncoder(model_type=model_type)
+        self.ffn = nn.Sequential([
+            nn.Linear(self.bert.feature_size, self.bert.feature_size),
+            nn.Tanh(),
+            nn.Dropout(p=output_dropout),
+            nn.Linear(self.bert.feature_size, 1),
+
+        ])
+
+    def forward(self, batch, device):
+        """
+        :param batch: tuple of (inputs_ids, labels)
+        :param device: GPU or CPU
+        :return: loss, num_rows, y_pred, targets
+        """
+        inputs, targets = batch
+        inputs, num_rows = inputs.to(device), inputs.size(0)
+        last_hidden_state = self.bert(inputs)
+        logits = self.ffn(last_hidden_state).squeeze(-1)
+        loss = F.binary_cross_entropy_with_logits(logits, targets).cuda()
+        y_pred = self.compute_pred(logits)
+        return loss, num_rows, y_pred, targets.cpu().numpy()
+
+    @staticmethod
+    def compute_pred(logits, threshold=.5):
+        y_pred = torch.sigmoid(logits) > threshold
+        return y_pred.float().cpu().numpy()
